d9ccc4a764b5dc415634c963e8fc9240ea9b6351
RoshanAtDure <roshan@duretechnologies.com>
2021-07-26 23:24:26 +0530

    Changes uerprfile to table list

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import Loader from "./loaders/loader";
 import service from "../Api/service";
 import Card from "react-bootstrap/Card";
+import Table from "react-bootstrap/Table";
+import Badge from "react-bootstrap/Badge";
 import { useHistory } from "react-router-dom";
 
 const Dashboard = () => {
@@ -34,7 +36,7 @@ const Dashboard = () => {
         <div>User List</div>
         <Loader isLoading={isLoading}></Loader>
         <div className="userListCard-container">
-          {userList.length > 0 && userList.map(userObj => {
+          {/* {userList.length > 0 && userList.map(userObj => {
             return (
               <Card className="userListCard" onClick={() => gotoprofile(userObj)}>
                 <Card.Body>
@@ -47,7 +49,39 @@ const Dashboard = () => {
                 </Card.Body>
               </Card>
             )
-          })}
+          })} */}
+          {
+            userList.length > 0 &&
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Gender</th>
+                  <th>Number</th>
+                  <th>Profile</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  userList.map((userObj, i) => {
+                    return (<tr>
+                      <td>{i+1}</td>
+                      <td>{userObj.name}</td>
+                      <td>{userObj.email}</td>
+                      <td>{userObj.gender}</td>
+                      <td>{userObj.contact_number}</td>
+                      <td><Badge pill bg="primary" onClick={() => gotoprofile(userObj)}>view</Badge></td>
+                    </tr>
+                    )
+                  })
+                }
+              </tbody>
+            </Table>
+
+          }
+
         </div>
       </div>
     </>
