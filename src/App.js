0508720e4bcb98679801609da29595e7a470414b
RoshanAtDure <roshan@duretechnologies.com>
2021-07-26 18:17:15 +0530

    Minor changes for navbar, login page.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 
+import React, { useState } from 'react';
 import { BrowserRouter, HashRouter, Route, Switch, Redirect, Link } from 'react-router-dom';
 import './App.css';
 import Login from "./components/login";
 import SignUp from "./components/signup";
 
-function App() {
+const App = () => {
+  
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
   return (
     <BrowserRouter>
       <div className="App">
         <nav className="navbar navbar-expand-lg navbar-light fixed-top">
           <div className="container">
             {/* <Link className="navbar-brand" to={"/sign-in"}>ABC</Link> */}
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <button class="custom-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation" onClick={handleNavCollapse}>
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarTogglerDemo02">
               <ul className="navbar-nav ml-auto">
                 <li className="nav-item">
                   <Link className="nav-link" to={"/sign-in"}>Login</Link>
@@ -28,8 +36,8 @@ function App() {
           <div className="auth-inner">
             <Switch>
               <Route exact path='/' component={Login} />
-                <Route path="/sign-in" component={Login} />
-                <Route path="/register" component={SignUp} />
+              <Route path="/sign-in" component={Login} />
+              <Route path="/register" component={SignUp} />
             </Switch>
           </div>
         </div>
--- src/components/login.js
+++ src/components/login.js
@@ -2,52 +2,59 @@ import React from "react";
 import { useForm } from "react-hook-form";
 
 function Login() {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const [errorMsg, setMessagw] = React.useState('')
+  const [isError, setError] = React.useState(false)
 
-    const onSubmit = (data) => {
-        console.log(data)
-        var reglocalList = JSON.parse(localStorage.getItem('reglocalList'));
-        let checkuserpresnt = reglocalList.filter(obj => obj.Email == data.Email && obj.Password == data.Password)
-        console.log(checkuserpresnt)
-        if (checkuserpresnt.length > 0) {
-        }
+  const onSubmit = (data) => {
+    console.log(data)
+    setError(false)
+    var reglocalList = JSON.parse(localStorage.getItem('reglocalList'));
+    let checkuserpresnt = reglocalList.filter(obj => obj.Email == data.Email && obj.Password == data.Password)
+    console.log(checkuserpresnt)
+    if (checkuserpresnt.length > 0) {
+
+    } else {
+      setError(true)
+      setMessagw('Email or password is incorrect!')
     }
+  }
 
-    return (
-        <>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                {/* register your input into the hook by invoking the "register" function */}
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input className="form-control" placeholder="Enter email"
-                        {...register("Email", {
-                            required: true,
-                            required: 'Email is required',
-                            pattern: {
-                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                                message: "invalid email address"
-                            }
-                        })} />
-                    {console.log(errors.Email)}
-                    {errors.Email && <span className="text-danger">{errors.Email.message ? errors.Email.message : "Please enter valid email id"}</span>}
-                </div>
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+
+        {/* register your input into the hook by invoking the "register" function */}
+        <div className="form-group">
+          <label>Email address:</label>
+          <input className="form-control" placeholder="Enter email"
+            {...register("Email", {
+              required: true,
+              required: 'Email is required',
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "invalid email address"
+              }
+            })} />
+          {console.log(errors.Email)}
+          {errors.Email && <span className="text-danger">{errors.Email.message ? errors.Email.message : "Please enter valid email id"}</span>}
+        </div>
 
 
-                {/* include validation with required or other standard HTML validation rules */}
-                <div className="form-group">
-                    <label>Password</label>
-                    <input className="form-control" placeholder="Enter password" {...register("Password", { required: true })} />
-                    {/* errors will return when field validation fails  */}
-                    {errors.Password && <span className="text-danger">This field is required</span>}
-                </div>
+        {/* include validation with required or other standard HTML validation rules */}
+        <div className="form-group">
+          <label>Password:</label>
+          <input className="form-control" type="password" placeholder="Enter password" {...register("Password", { required: true })} />
+          {/* errors will return when field validation fails  */}
+          {errors.Password && <span className="text-danger">Password is required</span>}
+        </div>
 
-                <input type="submit" />
-                <p className="forgot-password text-right">
-                    Not user?   <a href="/register">registered</a>
-                </p>
-            </form>
-        </>
-    )
+        {isError && <div className="text-danger">{errorMsg}</div>}
+        <input type="submit" />
+        <p className="forgot-password text-right">Not user?   <a href="/register">registered</a></p>
+      </form>
+    </>
+  )
 }
 
 export default Login
